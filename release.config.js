e49e4d79ea5c76ee714c88b3497d132741f53828
nonamich <dimaeromin97@gmail.com>
2025-02-03 07:24:03 +0200

    fix publish

--- release.config.js
+++ release.config.js
@@ -4,8 +4,12 @@ module.exports = {
     { name: 'alpha', prerelease: true },
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+    ['@semantic-release/exec', { publishCmd: 'echo "::set-output name=release_version::${nextRelease.version}"' }],
   ],
 };
