5355057f9dd4995da2950cdc25a01ae20a379065
nonamich <dimaeromin97@gmail.com>
2025-02-03 07:09:26 +0200

    fix publish

--- release.config.js
+++ release.config.js
@@ -8,8 +8,6 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/github',
-    '@semantic-release/git',
     ['@semantic-release/exec', { publishCmd: 'echo "::set-output name=release_version::${nextRelease.version}"' }],
   ],
 };
